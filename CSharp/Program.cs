75cc1baa6d3b3b4c6a04b3e02b2d0617cf50c613
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-24 12:47:50 -0300

    foreach loops

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -60,6 +60,29 @@ namespace CSharp
 
 			System.Console.WriteLine("Length of Math Students is {0}",  gradesOfMathStudents.Length);
 
+			int[] nums = new int[10];
+			for(int i=0; i<10; i++)
+			{
+				nums[i] = i;
+			}
+
+			for(int j=0; j<nums.Length; j++)
+			{
+				System.Console.WriteLine("Element{0} = {1}", j, nums[j]);
+			}
+
+			int counter = 0;
+			foreach(int k in nums)
+			{
+				System.Console.WriteLine("Element{0} = {1}", counter, k);
+				counter++;
+			}
+
+			string[] bestFriends = {"Mauro", "Fede", "Lucas", "Marce", "Fran"};
+			foreach(string name in bestFriends)
+			{
+				System.Console.WriteLine("Greetings my friend {0}", name);
+			}
 
 
         }
