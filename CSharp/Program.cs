3d8c4bfd10ee5809249c041ca836bdc5bd6db937
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-10-24 12:36:15 -0300

    arrays

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -25,7 +25,7 @@ namespace CSharp
 			basic.IntroduceMyself();
 
 			// BOX CLASS
-			Console.WriteLine("BOX CLASS TEST");
+			Console.WriteLine("BOX CLASS TESTING");
 			Box box = new(3, 4, 5);
 
             box.DisplayInfo();
@@ -36,9 +36,31 @@ namespace CSharp
 			Console.WriteLine("Box width is " + box.Width);
 
 			// MEMBER CLASS
-			Console.WriteLine(value: "MEMBER CLASS TEST");
+			Console.WriteLine(value: "MEMBER CLASS TESTING");
 			Members member1 = new Members();
 			member1.Introducing(true);
+
+			// ARRAYS
+			// The elements must be of the same datatype and you must specify it when you declare it
+			Console.WriteLine(value: "ARRAYS TESTING");
+			int[] grades = new int[5]; // datatype[] arrayName = new datatype[amount of values]
+			grades[0] = 20; // index 0 is equal to 20
+			grades[1] = 15;
+			grades[2] = 12;
+			grades[3] = 9;
+			grades[4] = 7;
+
+			System.Console.WriteLine("grades at index 0 : {0}", grades[0]);
+			
+			// another way of initialazing an array
+			int[] gradesOfMathStudents = {20, 32, 12, 8, 9};
+
+			// third way
+			int[] gradesOfHistoryStudents = new int[] {15, 20, 3, 17, 18, 15};
+
+			System.Console.WriteLine("Length of Math Students is {0}",  gradesOfMathStudents.Length);
+
+
 
         }
 
